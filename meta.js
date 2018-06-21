b28c3a1f263c84e1a448fd3c053f2727abd09ccf
uni <unibtc@gmail.com>
2018-06-22 02:44:21 +0800

    update

--- meta.js
+++ meta.js
@@ -19,6 +19,22 @@ module.exports = {
     author: {
       'type': 'string',
       'message': 'Author'
+    },
+    mode: {
+      'type': 'list',
+      'message': 'Nuxt build mode',
+      'choices': [
+        {
+          'name': 'Single Page Application',
+          'value': 'spa',
+          'short': 'spa'
+        },
+        {
+          'name': 'Isomorphic application(universal)',
+          'value': 'universal',
+          'short': 'universal'
+        }
+      ]
     }
   },
   completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
